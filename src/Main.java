baa7a07336b5f9781a6f96e71ae02f9afe37fab8
Dorival Ramos <dorivalmillan2@hotmail.com>
2025-08-13 20:28:54 -0300

    Inicio da Recordação De conhecimento em java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) {
+        String ola = "olá ";
+        String mundo = "Mundo de numero ";
+        String olaMundo = ola + mundo;
+
+        int um = 1, dois = 2, sumNumbers = um + dois;
+        double decimal = 3.14f + sumNumbers;
+        boolean programa = true;
+
+        int totalPorcento = 100;
+        int metadePorcento = 50;
+        float finalPorcento = (float) metadePorcento / totalPorcento * 100f;
+        String certeza = "Quais as chances de você estar correto ? ";
+
+        System.out.println(olaMundo + decimal );
+        System.out.println("Você é um progrma ? " + programa);
+        System.out.println(certeza + finalPorcento + "%");
+    }
+}
