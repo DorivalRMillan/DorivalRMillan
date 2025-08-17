de1db475437754a36e79af10d408c496cb692421
Dorival Ramos <dorivalmillan2@hotmail.com>
2025-08-17 15:26:58 -0300

    Foi adicionado um Valor de media

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,21 @@ public class Main {
 
         int soma = 0;
         int quantidade = 0;
+        int media = 0;
         while (entrada >=0 ) {
             if (entrada > 0 ) {
             soma += entrada;
             quantidade ++;
         };
-
+            media = soma / quantidade;
             System.out.println("Digite outro numero: ");
             entrada = sc.nextInt();
 
-        }
+        };
 
         System.out.printf("Você digitou %d numeros positivos %n", quantidade);
         System.out.printf("A soma deles é %d %n", soma);
+        System.out.printf("A media desses numeros é %d %n", media);
 
         sc.close();
     }
